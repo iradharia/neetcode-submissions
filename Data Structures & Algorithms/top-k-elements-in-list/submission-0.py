48e7b1bb6cca53d68f0c7dac290a89c0d831ec76
iradharia <108150018+iradharia@users.noreply.github.com>
2026-07-30 09:53:55 -0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+"""
+use a dictionary as buckets for {key = number, value = sum}
+while adding the counts of numbers to dictionary, 
+check to see if the value is ever equal to the target and append to op
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = defaultdict(int)
+        output = []
+        for num in nums:
+            counts[num]+=1
+        
+        sorted_nums = sorted(counts.keys(), key = lambda num: counts[num], reverse = True)
+        return sorted_nums[:k]
